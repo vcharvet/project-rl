aaafaa1c508d39a7a93efe1af471fc8f5f98f515
vcharvet <valentin.charvet@telecom-paristech.fr>
2019-01-27 18:09:04 +0100

    update architecture to DQN from DeepMind (2015)

--- neural_net.py
+++ neural_net.py
@@ -13,61 +13,39 @@ class QNetwork:
     def __init__(self, env, id, img_shape, h_size, out_size, lr=0.0001):
         img_row, img_col, nb_channels = img_shape
 
-        # initialization of weights and biases
-        # self.wc1 = tf.get_variable('wc1'+ id, shape=[3, 3, nb_channels, h_size],
-        #                            initializer=xavier_initializer())
-        # self.bc1 = tf.Variable(tf.zeros([h_size]), name='bc1'+id)
-        # self.wc2 = tf.get_variable('wc2'+id, shape=[3, 3, h_size, h_size],
-        #                            initializer=xavier_initializer())
-        # self.bc2 = tf.Variable(tf.zeros([h_size]), name='bc2'+id)
-        # self.wout = tf.Variable(tf.random.normal([h_size*h_size*nb_channels, out_size]),
-        #                         name='wout'+id)
-        # self.bout = tf.Variable(tf.zeros(out_size), name='bout'+id)
+        # input is a tensor of dimension 3
         self.X = tf.placeholder(shape=[None, img_row, img_col, nb_channels], dtype=tf.float32)
-        # self.conv1 = tf.nn.conv2d(self.X, self.wc1, padding='SAME', strides=[1, 1, 1, 1],
-        #                           name='conv1'+id, data_format='NHWC')
+
         self.conv1 = tf.layers.conv2d(inputs=self.X,
                                       filters=h_size,
-                                      kernel_size=[3, 3],
-                                      strides=[2, 2],
+                                      kernel_size=[8, 8],
+                                      strides=[4, 4],
                                       padding='SAME',
                                       kernel_initializer=xavier_initializer(),
                                       name='conv1'+id)
-        # self.z1 = tf.nn.dropout(tf.nn.relu(tf.add(self.conv1, self.bc1)), 0.75)
+
         self.z1 = tf.nn.relu(self.conv1, name='a1'+id)
 
-        # self.conv2 = tf.nn.conv2d(self.z1, self.wc2, padding='SAME', strides=[1, 2, 2, 1],
-        #                           name='conv2'+id, data_format='NHWC')
-        # self.a2 = tf.nn.dropout(tf.nn.relu(tf.add(self.conv2, self.bc2)), 0.75)
-        # self.z2 = tf.nn.max_pool(self.a2, ksize=[1, 2, 2, 1],
-        #                          strides=[1, 2, 2, 1], padding='SAME', name='maxpool2'+id)
         self.conv2 = tf.layers.conv2d(self.z1,
-                                      filters=h_size,
-                                      kernel_size=[3, 3],
+                                      filters=2*h_size,
+                                      kernel_size=[4, 4],
                                       strides=[2, 2],
                                       padding='SAME',
                                       kernel_initializer=xavier_initializer(),
                                       name='conv2'+id)
         self.z2 = tf.nn.relu(self.conv2, name='a2'+id)
 
-        # self.aout = tf.matmul(tf.reshape(self.z2, [-1, self.wout.shape[0]]), self.wout)
-        # self.zout = tf.add(self.aout, self.aout)
         self.conv3 = tf.layers.conv2d(self.z2,
                                       filters=2*h_size,
                                       kernel_size=[3, 3],
-                                      strides=[2, 2],
+                                      strides=[1, 1],
                                       padding='SAME',
                                       name='conv3'+id)
         self.z3 = tf.nn.relu(self.conv3, name='a2'+id)
 
-        #split the output from the final layer between advantage and value streams
-        # self.streamAC, self.streamVC = tf.split(self.zout, 2, 1)
+        # splitting the network for DDQN
         self.streamA = tf.layers.flatten(self.z3)
         self.streamV = tf.layers.flatten(self.z3)
-        # self.AW = tf.get_variable('AW'+id, shape=[out_size //2, env.action_space.n],
-        #                           initializer=xavier_initializer())
-        # self.VW = tf.get_variable('VW'+id, shape=[out_size//2, 1],
-        #                          initializer=xavier_initializer())
         self.AW = tf.layers.dense(inputs=self.streamA, units=out_size,
                                   activation=tf.nn.relu,
                                   kernel_initializer=xavier_initializer(),
