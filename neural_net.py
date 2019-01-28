bd599a6dd599c9fc3f79958c8a798cc82fabee7a
vcharvet <valentin.charvet@telecom-paristech.fr>
2019-01-28 15:18:38 +0100

    update architecture and training procedure

--- neural_net.py
+++ neural_net.py
@@ -9,7 +9,7 @@ import random
 import gym
 
 
-class QNetwork:
+class DoubleQNetwork:
     def __init__(self, env, id, img_shape, h_size, out_size, lr=0.0001):
         img_row, img_col, nb_channels = img_shape
 
@@ -18,34 +18,46 @@ class QNetwork:
 
         self.conv1 = tf.layers.conv2d(inputs=self.X,
                                       filters=h_size,
-                                      kernel_size=[8, 8],
-                                      strides=[4, 4],
+                                      kernel_size=[7, 7],
+                                      strides=[3, 3],
                                       padding='SAME',
                                       kernel_initializer=xavier_initializer(),
                                       name='conv1'+id)
-
-        self.z1 = tf.nn.relu(self.conv1, name='a1'+id)
+        self.a1 = tf.layers.batch_normalization(tf.nn.dropout(self.conv1, 0.5))
+        self.z1 = tf.nn.relu(self.a1, name='a1'+id)
 
         self.conv2 = tf.layers.conv2d(self.z1,
                                       filters=2*h_size,
-                                      kernel_size=[4, 4],
+                                      kernel_size=[5, 5],
                                       strides=[2, 2],
                                       padding='SAME',
                                       kernel_initializer=xavier_initializer(),
                                       name='conv2'+id)
-        self.z2 = tf.nn.relu(self.conv2, name='a2'+id)
+        self.a2 = tf.layers.batch_normalization(tf.nn.dropout(self.conv2, 0.5))
+        self.z2 = tf.nn.relu(self.a2, name='a2'+id)
 
         self.conv3 = tf.layers.conv2d(self.z2,
-                                      filters=2*h_size,
+                                      filters=h_size,
                                       kernel_size=[3, 3],
                                       strides=[1, 1],
                                       padding='SAME',
                                       name='conv3'+id)
-        self.z3 = tf.nn.relu(self.conv3, name='a2'+id)
+        self.a3 = tf.layers.batch_normalization(tf.nn.dropout(self.conv3, 0.5))
+        self.z3 = tf.nn.relu(self.a3, name='z3'+id)
+
+        self.conv4 = tf.layers.conv2d(self.z3,
+                                      filters=h_size,
+                                      kernel_size=[3, 3],
+                                      strides=[1, 1],
+                                      padding='SAME',
+                                      kernel_initializer=xavier_initializer(),
+                                      name='conv4'+id)
+        self.a4 = tf.layers.batch_normalization(tf.nn.dropout(self.conv4, 0.5))
+        self.z4 = tf.nn.relu(self.a4, name='z4'+id)
 
         # splitting the network for DDQN
-        self.streamA = tf.layers.flatten(self.z3)
-        self.streamV = tf.layers.flatten(self.z3)
+        self.streamA = tf.layers.flatten(self.z4)
+        self.streamV = tf.layers.flatten(self.z4)
         self.AW = tf.layers.dense(inputs=self.streamA, units=out_size,
                                   activation=tf.nn.relu,
                                   kernel_initializer=xavier_initializer(),
@@ -71,8 +83,10 @@ class QNetwork:
                                                              keepdims=True))
         self.predict = tf.argmax(self.Qout, 1)
 
+        # target inputs
         self.targetQ = tf.placeholder(shape=[None], dtype=tf.float32)
-        self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+        self.actions = tf.placeholder(shape=[None],
+                                      dtype=tf.int32)
         self.actions_onehot = tf.one_hot(self.actions, env.action_space.n,
                                          dtype=tf.float32)
 
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-from neural_net import QNetwork
+from neural_net import DoubleQNetwork
 from buffer import experience_buffer
 #import tf.layers.{dense, conv2d
 import os
@@ -23,15 +23,17 @@ def update_target(op_holder, sess):
         sess.run(op)
 
 
-def train_dqn(env, img_shape, batch_size=32, update_freq=4,  y=0.99, startE=1, endE=0.1,
-              annealing_steps=1000, num_episodes=10000, pre_train_episodes=100,
-              max_ep_length=50, load_model=False, save_path='./data/dqn',
-              lr=0.0001, h_size=64, out_size=512, render=False, verbosity=20,
-              tau=0.001):
+def train_double_dqn(env, img_shape, batch_size=32, update_freq=4,  y=0.99,
+                     startE=0.7,endE=0.95, annealing_steps=1000,
+                     num_episodes=10000, max_ep_length=50,
+                     pre_train_steps=10,
+                     load_model=False, save_path='./data/dqn',
+                     lr=0.0001, h_size=64, out_size=64, render=False,
+                     verbosity=20, tau=0.001):
     tf.reset_default_graph()
     # img_shape = 250, 160, 3
-    mainQN = QNetwork(env, 'main', img_shape, h_size, out_size, lr)
-    targetQN = QNetwork(env, 'target', img_shape, h_size, out_size, lr)
+    mainQN = DoubleQNetwork(env, 'main', img_shape, h_size, out_size, lr)
+    targetQN = DoubleQNetwork(env, 'target', img_shape, h_size, out_size, lr)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -41,7 +43,7 @@ def train_dqn(env, img_shape, batch_size=32, update_freq=4,  y=0.99, startE=1, e
     my_buffer = experience_buffer()
 
     e = startE
-    step_drop = (startE - endE) / annealing_steps
+    step_drop = (endE - startE) / annealing_steps
 
     jList, rList, total_steps = [], [], 0
 
@@ -54,60 +56,91 @@ def train_dqn(env, img_shape, batch_size=32, update_freq=4,  y=0.99, startE=1, e
             print('Loading model...')
             ckpt = tf.train.get_checkpoint_state(save_path)
             saver.restore(sess, ckpt.model_checkpoint_path)
-        for i in range(num_episodes):
+
+        # pretraining steps: random actions
+        print('Performing random pretraining steps...')
+        for i in range(pre_train_steps):
             episode_buffer = experience_buffer()
             s = env.reset()
-            s = s.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
+            # s = s.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
+            j = 0
+            while j < max_ep_length:
+                j+=1
+                a = np.random.randint(0, env.action_space.n)
+
+                s1, r, d, info = env.step(a)
+                # s1 = s1.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
+                episode_buffer.add(np.reshape(np.array([s, a, r, s1, d]), [1, 5]))
+                s = s1
+
+                if d:
+                    break
+
+            my_buffer.add(episode_buffer.buffer )
+        print('Random steps done\n Begin training...')
+
+        for i in range(num_episodes):
+            if e < endE:
+                e += step_drop
+
+            episode_buffer = experience_buffer()
+            s = env.reset()
+            # s = s.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
             rAll, j = 0, 0
             while j < max_ep_length:
                 j +=1
-                if np.random.rand(1) < e or i < pre_train_episodes:
+
+                if np.random.rand(1)[0] > e:
                     a = np.random.randint(0, env.action_space.n)
                 else:
-                    a = sess.run(mainQN.predict, feed_dict={mainQN.X: s})[0]
+                    s0 = s.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
+                    a = sess.run(mainQN.predict, feed_dict={mainQN.X: s0})[0]
 
                 s1, r, d, info = env.step(a)
                 total_steps +=1
                 episode_buffer.add(np.reshape(np.array([s, a, r, s1, d]), [1, 5]))
+                s = s1#.reshape((-1, img_shape[0], img_shape[1], img_shape[2]))
 
                 if render:
                     env.render()
-                if i > pre_train_episodes:
-                    if e > endE:
-                        e -= step_drop
-                    if total_steps % (update_freq) == 0:
-                        train_batch = my_buffer.sample(batch_size)
 
-                        states = np.vstack(train_batch[:, 3]).reshape((batch_size, img_shape[0],
-                                                            img_shape[1], img_shape[2]))
-                        Q1 = sess.run(mainQN.predict,
-                                      feed_dict={mainQN.X: states})
-                        Q2 = sess.run(targetQN.Qout,
-                                      feed_dict={targetQN.X: states})
-                        end_multiplier = -(train_batch[:, 4] - 1)
-                        doubleQ = Q2[:batch_size, Q1]
-                        targetQ = train_batch[:, 2] + (y*doubleQ.dot(end_multiplier))
+                if total_steps % (update_freq) == 0:
+                    train_batch = my_buffer.sample(batch_size)
 
-                        old_states = np.vstack(train_batch[:, 0]).reshape((batch_size, img_shape[0],
-                                                            img_shape[1], img_shape[2]))
-                        _, loss = sess.run([mainQN.update_model, mainQN.loss], feed_dict= \
-                            {mainQN.X: old_states, mainQN.targetQ: targetQ,
-                             mainQN.actions: train_batch[:, 1]})
-                        # print('Step {} on iter {}: need to update the main model '
-                        #       'with loss={:.3f}'.format(j, i, loss))
+                    states = np.vstack(train_batch[:, 3]).reshape(
+                        (batch_size, img_shape[0],
+                         img_shape[1], img_shape[2]))
+                    Q1 = sess.run(mainQN.predict,
+                                  feed_dict={mainQN.X: states})
+                    Q2 = sess.run(targetQN.Qout,
+                                  feed_dict={targetQN.X: states})
+                    end_multiplier = -(train_batch[:, 4] - 1)
+                    doubleQ = Q2[:batch_size, Q1]
+                    targetQ = train_batch[:, 2] + (
+                            y * doubleQ.dot(end_multiplier))
 
-                        update_target(target_ops, sess)
-                # print("Finish iteration {}, return={}"\
-                #       .format(i, r))
+                    old_states = np.vstack(train_batch[:, 0]).reshape(
+                        (batch_size, img_shape[0],
+                         img_shape[1], img_shape[2]))
+                    _, loss = sess.run([mainQN.update_model, mainQN.loss],
+                                       feed_dict= \
+                                           {mainQN.X: old_states,
+                                            mainQN.targetQ: targetQ,
+                                            mainQN.actions: train_batch[:, 1]})
+                    # print('Step {} on iter {}: need to update the main model '
+                    #       'with loss={:.3f}'.format(j, i, loss))
+
+                    update_target(target_ops, sess)
                 rAll+=r
                 if d:
                     print('Arriving on terminal state in iteration {}'.format(i))
                     break
-            print('Episode terminated in {} steps'.format(j))
+
+            print('Episode terminated in {} steps --- curent e:{}'.format(j, e))
             my_buffer.add(episode_buffer.buffer)
             jList.append(j)
             rList.append(rAll)
-            print('Reward of  iteration {}: {:.3f}\n'.format(i, rAll/j))
+            print('Reward of  iteration {}: {:.3f}\n'.format(i, rAll))
             if i % verbosity == 0:
                 print('On iteration {} mean return={:.3f} ---- total '
                       'steps:{}'.format(i, sum(rList)/(i+1), total_steps))
