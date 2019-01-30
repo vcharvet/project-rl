a84c0cd4601c0bd2b1305ebd84f486f84c1f6155
vcharvet <valentin.charvet@telecom-paristech.fr>
2019-01-30 11:22:27 +0100

    update videos: two segment in one window

--- videos.py
+++ videos.py
@@ -17,18 +17,18 @@ def choose_sample(segment1, segment2):
     #     ax1.set_title('Second Segment - {}th frame'.format(i))
     #     fig.show()
     axes = AxesSequence()
-    axes2 = AxesSequence
-    for i, ax in zip(range(segment1.shape[0]), axes):
-        ax.imshow(segment1[i, :, :, :])
-        ax.set_title('First Segment - {}th frame'.format(i))
+    for i in range(segment1.shape[0]):
+        ax1, ax2 = axes.new()
+        ax1.imshow(segment1[i, :, :, :])
+        ax1.set_title('First Segment - {}th frame'.format(i))
         # ax[1].imshow(segment2[i, :, :, :])
         # ax[1].set_title('Second Segment - {}th frame'.format(i))
-    for i, ax in zip(range(segment2.shape[0]), axes):
-        ax.imshow(segment2[i, :, :, :])
-        ax.set_title('Second Segment - {}th frame'.format(i))
+    # for i, ax in zip(range(segment2.shape[0]), axes):
+        ax2.imshow(segment2[i, :, :, :])
+        ax2.set_title('Second Segment - {}th frame'.format(i))
     axes.show()
 
-    user_preference = input("Choose either segment 1 or segment 2")
+    user_preference = input("Choose either segment 1 or segment 2:")
     plt.close('all')
     if user_preference == 1:
         res = 1, 0
@@ -44,7 +44,7 @@ class AxesSequence(object):
     """Creates a series of axes in a figure where only one is displayed at any
     given time. Which plot is displayed is controlled by the arrow keys."""
     def __init__(self):
-        self.fig = plt.figure()
+        self.fig = plt.figure(figsize=(16, 16))
         self.axes = []
         self._i = 0 # Currently displayed axes index
         self._n = 0 # Last created axes index
@@ -57,13 +57,13 @@ class AxesSequence(object):
     def new(self):
         # The label needs to be specified so that a new axes will be created
         # instead of "add_axes" just returning the original one.
-        ax = self.fig.add_axes([0.15, 0.1, 0.8, 0.8],
-                               visible=False, label=self._n)
-        # ax0 = self.fig.add_subplot(211)
-        # ax1 = self.fig.add_subplot(212)
+        # ax = self.fig.add_axes([0.15, 0.1, 0.8, 0.8],
+        #                        visible=False, label=self._n)
+        ax1 = self.fig.add_subplot(211, visible=False, label=self._n)
+        ax2 = self.fig.add_subplot(212, visible=False, label=self._n)
         self._n += 1
-        self.axes.append(ax)
-        return ax
+        self.axes.append((ax1, ax2))
+        return ax1, ax2
 
     def on_keypress(self, event):
         if event.key == 'right':
@@ -76,24 +76,24 @@ class AxesSequence(object):
 
     def next_plot(self):
         if self._i < len(self.axes):
-            self.axes[self._i].set_visible(False)
-            self.axes[self._i+1].set_visible(True)
+            self.axes[self._i][0].set_visible(False)
+            self.axes[self._i+1][0].set_visible(True)
 
-            # self.axes[self._i][1].set_visible(False)
-            # self.axes[self._i+1][1].set_visible(True)
+            self.axes[self._i][1].set_visible(False)
+            self.axes[self._i+1][1].set_visible(True)
             self._i += 1
 
     def prev_plot(self):
         if self._i > 0:
-            self.axes[self._i].set_visible(False)
-            self.axes[self._i-1].set_visible(True)
+            self.axes[self._i][0].set_visible(False)
+            self.axes[self._i-1][0].set_visible(True)
 
-            # self.axes[self._i][1].set_visible(False)
-            # self.axes[self._i-1][1].set_visible(True)
+            self.axes[self._i][1].set_visible(False)
+            self.axes[self._i-1][1].set_visible(True)
 
             self._i -= 1
 
     def show(self):
-        self.axes[0].set_visible(True)
-        # self.axes[0][1].set_visible(True)
+        self.axes[0][0].set_visible(True)
+        self.axes[0][1].set_visible(True)
         plt.show()
